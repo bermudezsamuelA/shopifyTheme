62cfd691aaaa7cb6fbab758604843357e9da872a
samuel <sbermudez_atenea@mercantilbanco.com.pa>
2022-07-30 00:03:20 -0400

    footer script

--- assets/apisConsume.js
+++ assets/apisConsume.js
@@ -1,13 +1,2 @@
 
     console.log("aqui")
-
-if(document.getElementById('pagefooter') != null){
-    console.log("aqui")
-    const url ='/admin/api/2022-07/blogs.json'
-    fetch(url)
-    .then((res)=> res.json())
-    .then((data)=>{
-        console.log('data',data)
-        document.getElementById('showBlogs')
-    })
-}
